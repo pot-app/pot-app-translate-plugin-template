67038bf21a8edf11f720afaf6a570000aa5f3b73
Pylogmon <pylogmon@outlook.com>
2023-09-03 13:59:22 +0800

    fix path

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cargo build --target "$INPUT_TARGET" --release
-mv target/$INPUT_TARGET/release/libplugin.so target/$INPUT_TARGET/release/plugin.so
+mv target/$INPUT_TARGET/release/libplugin.so ./plugin.so
