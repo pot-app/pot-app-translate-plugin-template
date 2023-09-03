2ff4c7991d553b6f43d4008b5ac7be3ccdaccc36
Pylogmon <pylogmon@outlook.com>
2023-09-03 11:38:25 +0800

    fix build

--- build-for-linux/entrypoint.sh
+++ build-for-linux/entrypoint.sh
@@ -9,7 +9,7 @@ if [ "$INPUT_TARGET" = "x86_64-unknown-linux-gnu" ]; then
 elif [ "$INPUT_TARGET" = "i686-unknown-linux-gnu" ]; then
     dpkg --add-architecture i386
     apt-get update
-    apt-get install -y pkg-config:i386 libssl-dev:i386
+    apt-get install -y libstdc++6:i386 libatomic1:i386 gcc-multilib g++-multilib pkg-config:i386 libssl-dev:i386
     export PKG_CONFIG_PATH=/usr/lib/i386-linux-gnu/pkgconfig/:$PKG_CONFIG_PATH
     export PKG_CONFIG_SYSROOT_DIR=/
 elif [ "$INPUT_TARGET" = "aarch64-unknown-linux-gnu" ]; then
@@ -17,7 +17,8 @@ elif [ "$INPUT_TARGET" = "aarch64-unknown-linux-gnu" ]; then
     sed -i 's/http:\/\/\(.*\).ubuntu.com\/ubuntu\//[arch=amd64,i386] http:\/\/\1.archive.ubuntu.com\/ubuntu\//g' /etc/apt/sources.list
     dpkg --add-architecture arm64
     apt-get update
-    apt-get install -y pkg-config:arm64 libssl-dev:arm64
+    apt-get install -y libncurses6:arm64 libtinfo6:arm64 linux-libc-dev:arm64 libncursesw6:arm64 libcups2:arm64
+    apt-get install -y --no-install-recommends g++-aarch64-linux-gnu libc6-dev-arm64-cross libssl-dev:arm64 pkg-config:arm64
     export CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER=aarch64-linux-gnu-gcc
     export CC_aarch64_unknown_linux_gnu=aarch64-linux-gnu-gcc
     export CXX_aarch64_unknown_linux_gnu=aarch64-linux-gnu-g++
@@ -28,6 +29,8 @@ elif [ "$INPUT_TARGET" = "armv7-unknown-linux-gnueabihf" ]; then
     sed -i 's/http:\/\/\(.*\).ubuntu.com\/ubuntu\//[arch=amd64,i386] http:\/\/\1.archive.ubuntu.com\/ubuntu\//g' /etc/apt/sources.list
     dpkg --add-architecture armhf
     apt-get update
+    apt-get install -y libncurses6:armhf libtinfo6:armhf linux-libc-dev:armhf libncursesw6:armhf libcups2:armhf
+    apt-get install -y --no-install-recommends g++-arm-linux-gnueabihf libc6-dev-armhf-cross libssl-dev:armhf libwebkit2gtk-4.0-dev:armhf libgtk-3-dev:armhf patchelf:armhf librsvg2-dev:armhf pkg-config:armhf
     apt-get install -y pkg-config:armhf libssl-dev:armhf
     export CARGO_TARGET_ARMV7_UNKNOWN_LINUX_GNUEABIHF_LINKER=arm-linux-gnueabihf-gcc
     export CC_armv7_unknown_linux_gnueabihf=arm-linux-gnueabihf-gcc
--- build.sh
+++ build.sh
@@ -2,3 +2,4 @@
 
 cargo build --target "$INPUT_TARGET" --release
 mv target/"$INPUT_TARGET"/release/libplugin.so target/"$INPUT_TARGET"/release/plugin.so
+zip $(cat info.json | jq '.id' | tr -d '"').zip info.json icon.svg target/"$INPUT_TARGET"/release/plugin.so
