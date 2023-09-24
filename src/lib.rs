1eb30d8d30b0e242ea48c3b6ffbc4356b85e4640
Pylogmon <pylogmon@outlook.com>
2023-09-24 23:05:19 +0800

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,7 @@ pub fn translate(
     from: &str, // 源语言
     to: &str,   // 目标语言
     // (pot会根据info.json 中的 language 字段传入插件需要的语言代码，无需再次转换)
+    detect: &str, // 检测到的语言 (若使用 detect, 需要手动转换)
     needs: HashMap<String, String>, // 插件需要的其他参数,由info.json定义
 ) -> Result<Value, Box<dyn Error>> {
     let client = reqwest::blocking::ClientBuilder::new().build()?;
